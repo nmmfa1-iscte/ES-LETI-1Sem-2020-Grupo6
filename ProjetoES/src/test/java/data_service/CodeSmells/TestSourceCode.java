e4086c7230b5438963a7090409f6c483b2d5d882
pcmee-iscte <pcmee@iscte-iul.pt>
2020-11-11 21:39:14 +0000

    Start Junit5 Setup

---
+++ ProjetoES/src/test/java/data_service/CodeSmells/TestSourceCode.java
@@ -0,0 +1,232 @@
+/**
+ * 
+ */
+package data_service.CodeSmells;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author pedro
+ *
+ */
+class TestSourceCode {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+	}
+
+	/**
+	 * Test method for {@link data_service.CodeSmells.SourceCode#getMethodID()}.
+	 */
+	@Test
+	void testGetMethodID() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link data_service.CodeSmells.SourceCode#setMethodID(int)}.
+	 */
+	@Test
+	void testSetMethodID() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link data_service.CodeSmells.SourceCode#getPkg()}.
+	 */
+	@Test
+	void testGetPkg() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link data_service.CodeSmells.SourceCode#setPkg(java.lang.String)}.
+	 */
+	@Test
+	void testSetPkg() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link data_service.CodeSmells.SourceCode#getClss()}.
+	 */
+	@Test
+	void testGetClss() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link data_service.CodeSmells.SourceCode#setClss(java.lang.String)}.
+	 */
+	@Test
+	void testSetClss() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link data_service.CodeSmells.SourceCode#getMethod()}.
+	 */
+	@Test
+	void testGetMethod() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link data_service.CodeSmells.SourceCode#setMethod(java.lang.String)}.
+	 */
+	@Test
+	void testSetMethod() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link data_service.CodeSmells.SourceCode#getLOC()}.
+	 */
+	@Test
+	void testGetLOC() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link data_service.CodeSmells.SourceCode#setLOC(int)}.
+	 */
+	@Test
+	void testSetLOC() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link data_service.CodeSmells.SourceCode#getCYCLO()}.
+	 */
+	@Test
+	void testGetCYCLO() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link data_service.CodeSmells.SourceCode#setCYCLO(int)}.
+	 */
+	@Test
+	void testSetCYCLO() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link data_service.CodeSmells.SourceCode#getATFD()}.
+	 */
+	@Test
+	void testGetATFD() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link data_service.CodeSmells.SourceCode#setATFD(int)}.
+	 */
+	@Test
+	void testSetATFD() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link data_service.CodeSmells.SourceCode#getLAA()}.
+	 */
+	@Test
+	void testGetLAA() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link data_service.CodeSmells.SourceCode#setLAA(org.apache.poi.ss.usermodel.Cell)}.
+	 */
+	@Test
+	void testSetLAA() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link data_service.CodeSmells.SourceCode#getIs_long_method()}.
+	 */
+	@Test
+	void testGetIs_long_method() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link data_service.CodeSmells.SourceCode#setIs_long_method(org.apache.poi.ss.usermodel.Cell)}.
+	 */
+	@Test
+	void testSetIs_long_method() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link data_service.CodeSmells.SourceCode#getiPlasma()}.
+	 */
+	@Test
+	void testGetiPlasma() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link data_service.CodeSmells.SourceCode#setiPlasma(org.apache.poi.ss.usermodel.Cell)}.
+	 */
+	@Test
+	void testSetiPlasma() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link data_service.CodeSmells.SourceCode#getPMD()}.
+	 */
+	@Test
+	void testGetPMD() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link data_service.CodeSmells.SourceCode#setPMD(org.apache.poi.ss.usermodel.Cell)}.
+	 */
+	@Test
+	void testSetPMD() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link data_service.CodeSmells.SourceCode#getIs_featue_envy()}.
+	 */
+	@Test
+	void testGetIs_featue_envy() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link data_service.CodeSmells.SourceCode#setIs_featue_envy(org.apache.poi.ss.usermodel.Cell)}.
+	 */
+	@Test
+	void testSetIs_featue_envy() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link data_service.CodeSmells.SourceCode#toString()}.
+	 */
+	@Test
+	void testToString() {
+		fail("Not yet implemented");
+	}
+
+}
