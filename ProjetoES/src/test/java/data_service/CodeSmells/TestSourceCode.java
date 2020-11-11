a467d61e512ac2af5ef33f7087e10e0d8adca601
pcmee-iscte <pcmee@iscte-iul.pt>
2020-11-11 22:27:47 +0000

    Created data_service.CodeSmells package Created TestSouceCode.java
    First Tests

--- ProjetoES/src/test/java/data_service/CodeSmells/TestSourceCode.java
+++ ProjetoES/src/test/java/data_service/CodeSmells/TestSourceCode.java
@@ -5,6 +5,18 @@ package data_service.CodeSmells;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.Calendar;
+import java.util.Date;
+
+import org.apache.poi.ss.formula.FormulaParseException;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CellStyle;
+import org.apache.poi.ss.usermodel.Comment;
+import org.apache.poi.ss.usermodel.Hyperlink;
+import org.apache.poi.ss.usermodel.RichTextString;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.util.CellRangeAddress;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
@@ -14,12 +26,14 @@ import org.junit.jupiter.api.Test;
  *
  */
 class TestSourceCode {
+	static SourceCode souceCodeTest;
 
 	/**
 	 * @throws java.lang.Exception
 	 */
 	@BeforeAll
 	static void setUpBeforeClass() throws Exception {
+		souceCodeTest = new SourceCode();
 	}
 
 	/**
@@ -31,193 +45,118 @@ class TestSourceCode {
 
 	/**
 	 * Test method for {@link data_service.CodeSmells.SourceCode#getMethodID()}.
-	 */
-	@Test
-	void testGetMethodID() {
-		fail("Not yet implemented");
-	}
-
-	/**
 	 * Test method for {@link data_service.CodeSmells.SourceCode#setMethodID(int)}.
 	 */
 	@Test
-	void testSetMethodID() {
-		fail("Not yet implemented");
+	void testGetSetMethodID() {
+		souceCodeTest.setMethodID(0);
+		assertEquals(0, souceCodeTest.getMethodID());
 	}
 
 	/**
 	 * Test method for {@link data_service.CodeSmells.SourceCode#getPkg()}.
-	 */
-	@Test
-	void testGetPkg() {
-		fail("Not yet implemented");
-	}
-
-	/**
 	 * Test method for {@link data_service.CodeSmells.SourceCode#setPkg(java.lang.String)}.
 	 */
 	@Test
-	void testSetPkg() {
-		fail("Not yet implemented");
+	void testGetSetPkg() {
+		souceCodeTest.setPkg("Test");
+		assertEquals("Test", souceCodeTest.getPkg());
 	}
 
 	/**
 	 * Test method for {@link data_service.CodeSmells.SourceCode#getClss()}.
-	 */
-	@Test
-	void testGetClss() {
-		fail("Not yet implemented");
-	}
-
-	/**
 	 * Test method for {@link data_service.CodeSmells.SourceCode#setClss(java.lang.String)}.
 	 */
 	@Test
-	void testSetClss() {
-		fail("Not yet implemented");
+	void testGetSetClss() {
+		souceCodeTest.setClss("Test");
+		assertEquals("Test", souceCodeTest.getClss());
 	}
 
 	/**
 	 * Test method for {@link data_service.CodeSmells.SourceCode#getMethod()}.
-	 */
-	@Test
-	void testGetMethod() {
-		fail("Not yet implemented");
-	}
-
-	/**
 	 * Test method for {@link data_service.CodeSmells.SourceCode#setMethod(java.lang.String)}.
 	 */
 	@Test
-	void testSetMethod() {
-		fail("Not yet implemented");
+	void testGetSetMethod() {
+		souceCodeTest.setMethod("Test");
+		assertEquals("Test", souceCodeTest.getMethod());
 	}
 
 	/**
 	 * Test method for {@link data_service.CodeSmells.SourceCode#getLOC()}.
-	 */
-	@Test
-	void testGetLOC() {
-		fail("Not yet implemented");
-	}
-
-	/**
 	 * Test method for {@link data_service.CodeSmells.SourceCode#setLOC(int)}.
 	 */
 	@Test
-	void testSetLOC() {
-		fail("Not yet implemented");
+	void testGetSetLOC() {
+		souceCodeTest.setLOC(0);
+		assertEquals(0, souceCodeTest.getLOC());
 	}
 
 	/**
 	 * Test method for {@link data_service.CodeSmells.SourceCode#getCYCLO()}.
-	 */
-	@Test
-	void testGetCYCLO() {
-		fail("Not yet implemented");
-	}
-
-	/**
 	 * Test method for {@link data_service.CodeSmells.SourceCode#setCYCLO(int)}.
 	 */
 	@Test
-	void testSetCYCLO() {
-		fail("Not yet implemented");
+	void testGetSetCYCLO() {
+		souceCodeTest.setCYCLO(0);
+		assertEquals(0, souceCodeTest.getCYCLO());
 	}
+	
 
 	/**
 	 * Test method for {@link data_service.CodeSmells.SourceCode#getATFD()}.
-	 */
-	@Test
-	void testGetATFD() {
-		fail("Not yet implemented");
-	}
-
-	/**
 	 * Test method for {@link data_service.CodeSmells.SourceCode#setATFD(int)}.
 	 */
 	@Test
-	void testSetATFD() {
-		fail("Not yet implemented");
+	void testGetSetATFD() {
+		souceCodeTest.setATFD(0);
+		assertEquals(0, souceCodeTest.getATFD());
 	}
 
 	/**
 	 * Test method for {@link data_service.CodeSmells.SourceCode#getLAA()}.
-	 */
-	@Test
-	void testGetLAA() {
-		fail("Not yet implemented");
-	}
-
-	/**
 	 * Test method for {@link data_service.CodeSmells.SourceCode#setLAA(org.apache.poi.ss.usermodel.Cell)}.
 	 */
 	@Test
-	void testSetLAA() {
-		fail("Not yet implemented");
+	void testGetSetLAA() {
+		//souceCodeTest.setLAA(new Cell());
+		assertEquals(0, souceCodeTest.getLAA());
 	}
 
 	/**
 	 * Test method for {@link data_service.CodeSmells.SourceCode#getIs_long_method()}.
-	 */
-	@Test
-	void testGetIs_long_method() {
-		fail("Not yet implemented");
-	}
-
-	/**
 	 * Test method for {@link data_service.CodeSmells.SourceCode#setIs_long_method(org.apache.poi.ss.usermodel.Cell)}.
 	 */
 	@Test
-	void testSetIs_long_method() {
+	void testGetSetIs_long_method() {
 		fail("Not yet implemented");
 	}
 
 	/**
 	 * Test method for {@link data_service.CodeSmells.SourceCode#getiPlasma()}.
-	 */
-	@Test
-	void testGetiPlasma() {
-		fail("Not yet implemented");
-	}
-
-	/**
 	 * Test method for {@link data_service.CodeSmells.SourceCode#setiPlasma(org.apache.poi.ss.usermodel.Cell)}.
 	 */
 	@Test
-	void testSetiPlasma() {
+	void testGetSetiPlasma() {
 		fail("Not yet implemented");
 	}
 
 	/**
 	 * Test method for {@link data_service.CodeSmells.SourceCode#getPMD()}.
-	 */
-	@Test
-	void testGetPMD() {
-		fail("Not yet implemented");
-	}
-
-	/**
 	 * Test method for {@link data_service.CodeSmells.SourceCode#setPMD(org.apache.poi.ss.usermodel.Cell)}.
 	 */
 	@Test
-	void testSetPMD() {
+	void testGetSetPMD() {
 		fail("Not yet implemented");
 	}
 
 	/**
 	 * Test method for {@link data_service.CodeSmells.SourceCode#getIs_featue_envy()}.
-	 */
-	@Test
-	void testGetIs_featue_envy() {
-		fail("Not yet implemented");
-	}
-
-	/**
 	 * Test method for {@link data_service.CodeSmells.SourceCode#setIs_featue_envy(org.apache.poi.ss.usermodel.Cell)}.
 	 */
 	@Test
-	void testSetIs_featue_envy() {
+	void testGetSetIs_featue_envy() {
 		fail("Not yet implemented");
 	}
 
